124dd7eba5542e7d2e69398416959260c40a25c6
wrhalpin <194402614+wrhalpin@users.noreply.github.com>
2026-04-02 12:29:29 -0400

    removed superfluous dir, moved under security with newer versions

--- gnat/agents/secrets/hygiene/__init__.py
+++
@@ -1,9 +0,0 @@
-from .duplicate_detector import DuplicateSecretDetector
-from .leak_scanner import SecretLeakScanner
-from .unsafe_secrets import UnsafeSecretAnalyzer
-
-__all__ = [
-    "SecretLeakScanner",
-    "DuplicateSecretDetector",
-    "UnsafeSecretAnalyzer",
-]
--- gnat/agents/secrets/providers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .azure_key_vault import AzureKeyVaultProvider
-from .cyberark import CyberArkProvider
-from .memory import InMemorySecretsProvider
-
-__all__ = [
-    "AzureKeyVaultProvider",
-    "CyberArkProvider",
-    "InMemorySecretsProvider",
-]
